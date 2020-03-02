dfda5891068445373608104865392d4e49d5d7d3
[Molita Nuon] <[molitanuon36@csu.fullerton.edu]>
2020-03-01 17:40:12 -0800

    changes in sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -17,31 +17,55 @@ void* sharedMemPtr;
 
 /**
  * Sets up the shared memory segment and message queue
- * @param shmid - the id of the allocated shared memory 
+ * @param shmid - the id of the allocated shared memory
  * @param msqid - the id of the shared memory
  */
 
 void init(int& shmid, int& msqid, void*& sharedMemPtr)
 {
-	/* TODO: 
+	/* TODO:
         1. Create a file called keyfile.txt containing string "Hello world" (you may do
  		    so manually or from the code).
 	    2. Use ftok("keyfile.txt", 'a') in order to generate the key.
 		3. Use the key in the TODO's below. Use the same key for the queue
 		    and the shared memory segment. This also serves to illustrate the difference
 		    between the key and the id used in message queues and shared memory. The id
-		    for any System V objest (i.e. message queues, shared memory, and sempahores) 
+		    for any System V objest (i.e. message queues, shared memory, and sempahores)
 		    is unique system-wide among all SYstem V objects. Two objects, on the other hand,
 		    may have the same key.
 	 */
-	
 
-	
+	 key_t key = ftok("keyfile.txt", 'a');
+
+	 if(key < 0)
+	 {
+		 perror("ftok");
+		 exit(1);
+	 }
 	/* TODO: Get the id of the shared memory segment. The size of the segment must be SHARED_MEMORY_CHUNK_SIZE */
+	shmid = shmget(key, SHARED_MEMORY_CHUNK_SIZE, 0644);
+	if (shmid < 0)
+	{
+		perror("shmget");
+		exit(1);
+
+	}
 	/* TODO: Attach to the shared memory */
+	sharedMemPtr = shmat(shmid, NULL, 0);
+	if(sharedMemPtr < 0)
+	{
+		perror("shmat");
+		exit(1)
+	}
 	/* TODO: Attach to the message queue */
+	mqid = msgget(key, 0666);
+	if(msqiud < 0)
+	{
+		perror("msgget");
+		exit(1);
+	}
 	/* Store the IDs and the pointer to the shared memory region in the corresponding parameters */
-	
+
 }
 
 /**
@@ -54,6 +78,8 @@ void init(int& shmid, int& msqid, void*& sharedMemPtr)
 void cleanUp(const int& shmid, const int& msqid, void* sharedMemPtr)
 {
 	/* TODO: Detach from shared memory */
+	shmdt(sharedMemPtr);
+
 }
 
 /**
@@ -64,25 +90,25 @@ void send(const char* fileName)
 {
 	/* Open the file for reading */
 	FILE* fp = fopen(fileName, "r");
-	
+
 
 	/* A buffer to store message we will send to the receiver. */
-	message sndMsg; 
-	
+	message sndMsg;
+
 	/* A buffer to store message received from the receiver. */
 	message rcvMsg;
-	
+
 	/* Was the file open? */
 	if(!fp)
 	{
 		perror("fopen");
 		exit(-1);
 	}
-	
+
 	/* Read the whole file */
 	while(!feof(fp))
 	{
-		/* Read at most SHARED_MEMORY_CHUNK_SIZE from the file and store them in shared memory. 
+		/* Read at most SHARED_MEMORY_CHUNK_SIZE from the file and store them in shared memory.
  		 * fread will return how many bytes it has actually read (since the last chunk may be less
  		 * than SHARED_MEMORY_CHUNK_SIZE).
  		 */
@@ -91,48 +117,52 @@ void send(const char* fileName)
 			perror("fread");
 			exit(-1);
 		}
-		
-			
-		/* TODO: Send a message to the receiver telling him that the data is ready 
- 		 * (message of type SENDER_DATA_TYPE) 
+
+
+		/* TODO: Send a message to the receiver telling him that the data is ready
+ 		 * (message of type SENDER_DATA_TYPE)
  		 */
-		
-		/* TODO: Wait until the receiver sends us a message of type RECV_DONE_TYPE telling us 
- 		 * that he finished saving the memory chunk. 
+		 sndMessage.mtype = SENDER_DATA_TYPE;
+		 msgsnd(msqid, &sndMsg, sizeof(sndMessage) - sizeof(long), 0);
+
+		/* TODO: Wait until the receiver sends us a message of type RECV_DONE_TYPE telling us
+ 		 * that he finished saving the memory chunk.
  		 */
+			msgrcv(msqid, &rvcMsg, 0, RECV_DONE_TYPE, 0);
 	}
-	
+
 
 	/** TODO: once we are out of the above loop, we have finished sending the file.
  	  * Lets tell the receiver that we have nothing more to send. We will do this by
- 	  * sending a message of type SENDER_DATA_TYPE with size field set to 0. 	
+ 	  * sending a message of type SENDER_DATA_TYPE with size field set to 0.
 	  */
+			sndMessage.size = 0;
+			msgsnd(msqid, &sndMsg, sizeof(sndMessage) - sizeof(long), 0);
 
-		
 	/* Close the file */
 	fclose(fp);
-	
+
 }
 
 
 int main(int argc, char** argv)
 {
-	
+
 	/* Check the command line arguments */
 	if(argc < 2)
 	{
 		fprintf(stderr, "USAGE: %s <FILE NAME>\n", argv[0]);
 		exit(-1);
 	}
-		
+
 	/* Connect to shared memory and the message queue */
 	init(shmid, msqid, sharedMemPtr);
-	
+
 	/* Send the file */
 	send(argv[1]);
-	
+
 	/* Cleanup */
 	cleanUp(shmid, msqid, sharedMemPtr);
-		
+
 	return 0;
 }
